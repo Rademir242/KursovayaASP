1a8d650544f52d76f52dcf111e9fe6e4b0a66a97
Rademir242 <sultanoraev@mail.ru>
2026-04-02 14:40:35 +0500

    Add files via upload

---
+++ GamingCatalogue/Models/AssignedTagData.cs
@@ -0,0 +1,9 @@
+﻿namespace GamingCatalogue.Models
+{
+    public class AssignedTagData
+    {
+        public int TagId { get; set; }
+        public string Name { get; set; }
+        public bool Assigned { get; set; } 
+    }
+}
---
+++ GamingCatalogue/Models/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace GamingCatalogue.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Game> Games { get; set; } = new List<Game>();
+    }
+}
