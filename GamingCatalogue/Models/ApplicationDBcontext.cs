54f9ff4781105fbe16b8929c78aadde7eefcadc0
Rademir242 <sultanoraev@mail.ru>
2026-04-13 14:02:55 +0500

    ApplicationDBcontext.cs

--- GamingCatalogue/Models/ApplicationDBcontext.cs
+++ GamingCatalogue/Models/ApplicationDBcontext.cs
@@ -10,6 +10,7 @@ public class ApplicationDbContext : DbContext
     }
     public DbSet<Game> Games { get; set; }
     public DbSet<Tag> Tags { get; set; }
+    public DbSet<Genre> Genres { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
