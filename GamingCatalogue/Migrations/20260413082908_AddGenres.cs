9e3a1b47d585e307556b9e581fe8024701b21029
Rademir242 <sultanoraev@mail.ru>
2026-04-13 14:00:26 +0500

    Add files via upload

--- GamingCatalogue/Migrations/20260413082908_AddGenres.cs
+++ GamingCatalogue/Migrations/20260413082908_AddGenres.cs
@@ -2,18 +2,16 @@
 
 #nullable disable
 
-#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
-
 namespace GamingCatalogue.Migrations
 {
     /// <inheritdoc />
-    public partial class AddGameTagRelation : Migration
+    public partial class AddGenres : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Tags",
+                name: "Genres",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
@@ -22,58 +20,47 @@ namespace GamingCatalogue.Migrations
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Tags", x => x.Id);
+                    table.PrimaryKey("PK_Genres", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
-                name: "GameTag",
+                name: "GameGenre",
                 columns: table => new
                 {
                     GamesId = table.Column<int>(type: "int", nullable: false),
-                    TagsId = table.Column<int>(type: "int", nullable: false)
+                    GenresId = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_GameTag", x => new { x.GamesId, x.TagsId });
+                    table.PrimaryKey("PK_GameGenre", x => new { x.GamesId, x.GenresId });
                     table.ForeignKey(
-                        name: "FK_GameTag_Games_GamesId",
+                        name: "FK_GameGenre_Games_GamesId",
                         column: x => x.GamesId,
                         principalTable: "Games",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
-                        name: "FK_GameTag_Tags_TagsId",
-                        column: x => x.TagsId,
-                        principalTable: "Tags",
+                        name: "FK_GameGenre_Genres_GenresId",
+                        column: x => x.GenresId,
+                        principalTable: "Genres",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.InsertData(
-                table: "Tags",
-                columns: new[] { "Id", "Name" },
-                values: new object[,]
-                {
-                    { 1, "Open World" },
-                    { 2, "RPG" },
-                    { 3, "First Person" },
-                    { 4, "Action" }
-                });
-
             migrationBuilder.CreateIndex(
-                name: "IX_GameTag_TagsId",
-                table: "GameTag",
-                column: "TagsId");
+                name: "IX_GameGenre_GenresId",
+                table: "GameGenre",
+                column: "GenresId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "GameTag");
-            
+                name: "GameGenre");
+
             migrationBuilder.DropTable(
-                name: "Tags");
+                name: "Genres");
         }
     }
 }
