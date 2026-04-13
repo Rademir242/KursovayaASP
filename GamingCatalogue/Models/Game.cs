efd2a627d0fca71b54022e6829b6f9dc6f6b4569
Rademir242 <sultanoraev@mail.ru>
2026-04-13 14:02:15 +0500

    Update Game.cs

--- GamingCatalogue/Models/Game.cs
+++ GamingCatalogue/Models/Game.cs
@@ -1,19 +1,22 @@
-﻿namespace GamingCatalogue.Models
+using GamingCatalogue.Models;
+
+namespace GamingCatalogue.Models
 {
-public class Game
+    public class Game
     {
-        public int Id { get; set; } 
-        public string Title { get; set; } 
-        public string Year { get; set; } 
-        public string Description { get; set; } 
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Description { get; set; }
 
         public double GameplayScore { get; set; }
         public double GraphicsScore { get; set; }
         public double StoryScore { get; set; }
         public double MusicScore { get; set; }
-
-
         public string ImageUrl { get; set; }
         public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+        public ICollection<Genre> Genres { get; set; } = new List<Genre>();
     }
+
+
 }
