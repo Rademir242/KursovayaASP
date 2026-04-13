6bdcbb5f8d1158d7203a48d29d12b7c97de66d72
Rademir242 <sultanoraev@mail.ru>
2026-04-13 14:01:35 +0500

    Add files via upload

---
+++ GamingCatalogue/Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GamingCatalogue.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Game> Games { get; set; } = new List<Game>();
+    }
+}
